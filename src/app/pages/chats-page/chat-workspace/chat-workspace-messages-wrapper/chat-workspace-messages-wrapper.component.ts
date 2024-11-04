d7cf8ac0d7257d949edff910f44b92354ecbc36f
Vlad Gvozdev <vlllad2011@gmail.com>
2024-11-04 23:07:08 +0300

    Optimized resize chat window

--- src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
@@ -1,9 +1,10 @@
-import {Component, inject, input, signal} from '@angular/core';
+import {Component, ElementRef, inject, input, Renderer2, ViewChild} from '@angular/core';
 import {ChatWorkspaceMessageComponent} from "./chat-workspace-message/chat-workspace-message.component";
 import {MessageInputComponent} from "../../../../common-ui/message-input/message-input.component";
 import {ChatsService} from "../../../../data/services/chats.service";
-import {Chat, Message} from "../../../../data/interfaces/chat.interface";
-import {firstValueFrom} from "rxjs";
+import {Chat} from "../../../../data/interfaces/chat.interface";
+import {audit, firstValueFrom, fromEvent, interval} from "rxjs";
+import {resolve} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-chat-workspace-messages-wrapper',
@@ -17,6 +18,8 @@ import {firstValueFrom} from "rxjs";
 })
 export class ChatWorkspaceMessagesWrapperComponent {
   chatsService = inject(ChatsService)
+  elRef = inject(ElementRef)
+  r2 = inject(Renderer2)
 
   chat = input.required<Chat>()
 
@@ -26,5 +29,38 @@ export class ChatWorkspaceMessagesWrapperComponent {
     await firstValueFrom(this.chatsService.sendMessage(this.chat().id, messageText))
 
     await firstValueFrom(this.chatsService.getChatById(this.chat().id))
+    
+    this.scrollBottom()
+  }
+
+  ngAfterViewInit() {
+    this.resizeMessages()
+    this.scrollBottom()
+    fromEvent(window, 'resize')
+        .pipe(
+            audit(() => interval(50))
+        )
+        .subscribe(() => {
+          this.resizeMessages()
+        })
+  }
+
+  ngAfterViewChecked() {
+    this.scrollBottom();
+  }
+
+  resizeMessages() {
+    const {top} = this.elRef.nativeElement.getBoundingClientRect()
+
+    const width = this.elRef.nativeElement.offsetWidth - 20
+
+    const height = window.innerHeight - top - 130
+
+    this.r2.setStyle(this.elRef.nativeElement, 'height', `${height}px`)
+    this.r2.setStyle(this.elRef.nativeElement.querySelector('app-message-input'), 'width', `${width}px`)
+  }
+
+  scrollBottom() {
+    this.elRef.nativeElement.scrollTop = this.elRef.nativeElement.scrollHeight
   }
 }
